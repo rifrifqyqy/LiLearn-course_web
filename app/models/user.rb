5a551b6a947b9c5ad8a29b7827d4988ab9005364
rifrifqyqy <rifqyh22@gmail.com>
2024-10-27 18:01:40 +0700

    sign in model changed from find to where. add validate password must match. attr_accessor for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,21 @@ class User
   field :username, type: String
   field :password, type: String
   field :role, type: String, default: 'user'   # Jika Anda menyimpan password secara langsung (tidak disarankan untuk keamanan)
+  attr_accessor :password_confirmation
 
   # Validasi unik untuk username
   validates :username, presence: true, uniqueness: true
-
+  # validasi password
+  validate :password_must_match_confirmation
   # Metode untuk memverifikasi password
+  def password_must_match_confirmation
+    if password != password_confirmation
+      errors.add(:password_confirmation, "Password tidak cocok. Pastikan keduanya sama.")
+    end
+  end
+  # varifikasi username must unique
   def self.authenticate(username, password)
-    user = User.find_by(username: username)
+    user = User.where(username: username).first
     return user if user && user.password == password
     nil
   end
