87fb51dd55511c51f16c1ae2d7abdacd83c608ca
rifrifqyqy <rifqyh22@gmail.com>
2024-11-02 01:47:58 +0700

    add last login models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User
 
   field :username, type: String
   field :password, type: String
-  field :role, type: String, default: 'user'   # Jika Anda menyimpan password secara langsung (tidak disarankan untuk keamanan)
+  field :role, type: String, default: 'user'  
+  field :last_login, type: Time
+   # Jika Anda menyimpan password secara langsung (tidak disarankan untuk keamanan)
   attr_accessor :password_confirmation
 
   # Validasi unik untuk username
@@ -18,10 +20,24 @@ class User
       errors.add(:password_confirmation, "Password tidak cocok. Pastikan keduanya sama.")
     end
   end
-  # varifikasi username must unique
-  def self.authenticate(username, password)
-    user = User.where(username: username).first
-    return user if user && user.password == password
-    nil
-  end
+ # Memperbarui `last_login` saat berhasil login
+    def self.authenticate(username, password)
+      user = User.where(username: username).first
+      if user && user.password == password
+        user.update_attributes(last_login: Time.current)
+          Rails.logger.info "Last login updated for user #{user.username} at #{user.last_login}"
+        return user
+      else
+        nil
+      end
+    end
+    def update_last_login
+      self.last_login = Time.current
+      # Gunakan update_attribute untuk menghindari validasi
+      if update_attribute(:last_login, last_login) 
+        puts "Last login updated successfully to #{last_login}"
+      else
+        puts "Failed to update last login: #{errors.full_messages.join(', ')}"
+      end
+    end
 end
