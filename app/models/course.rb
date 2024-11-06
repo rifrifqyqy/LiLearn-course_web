d8368cfe198b32d6b347de242d1471cf2d9324af
rifrifqyqy <rifqyh22@gmail.com>
2024-11-06 12:28:15 +0700

    add method for calculate average course rating

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,5 +10,24 @@ class Course
    accepts_nested_attributes_for :materials, allow_destroy: true
    # Pemasangan uploader thumbnail
    mount_uploader :thumbnail, ThumbnailUploader
-   
+   # Asosiasi dengan reviews
+   has_many :reviews, dependent: :destroy
+   accepts_nested_attributes_for :reviews, allow_destroy: true
+
+   # Menghitung rata-rata rating menggunakan agregasi Mongoid
+  def average_rating
+    return 'N/A' if reviews.empty?
+  
+    # Gunakan aggregate pada koleksi Review untuk menghitung rata-rata
+    result = Review.collection.aggregate([
+      { "$match" => { "course_id" => self.id } },
+      { "$group" => { "_id" => nil, "avg_rating" => { "$avg" => "$rating" } } }
+    ])
+  
+    # Ambil hasil rata-rata dan bulatkan jika diperlukan
+    avg_rating = result.first ? result.first['avg_rating'] : 0
+    # Jika rata-rata adalah bilangan bulat, kembalikan sebagai integer
+    avg_rating.to_i == avg_rating ? avg_rating.to_i : avg_rating.round(1)
+  end
+  
 end
