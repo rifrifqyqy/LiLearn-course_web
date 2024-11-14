99e8669de79d6ca5c30b3c8f8e13f0057dbe071c
rifrifqyqy <rifqyh22@gmail.com>
2024-11-14 20:02:27 +0700

    CRUD review for logged user | properly done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,48 @@
 class ReviewsController < ApplicationController
+  before_action :set_course
+  before_action :set_review, only: [:update, :destroy]
+# Controller ReviewsController
   def create
     @course = Course.find(params[:course_id])
-    @review = @course.reviews.build(review_params)
-    @review.user = current_user # Pastikan user sedang login jika diperlukan
-    
-    if @review.save
-      redirect_to @course, notice: 'Review successfully added.'
+    @review = @course.reviews.find_or_initialize_by(user: current_user)
+
+    if @review.new_record?
+      @review.assign_attributes(review_params)
+      if @review.save
+        redirect_to request.referer, notice: 'Review successfully added.'
+      else
+        redirect_to request.referer, alert: 'There was an error with your review.'
+      end
     else
-      redirect_to @course, alert: 'There was an error with your review.'
+      # Jika review sudah ada
+      redirect_to request.referer, alert: 'You have already rated this course.'
     end
   end
 
-  private
 
+   # Mengupdate review yang sudah ada
+  def update
+    if @review.update(review_params)
+      redirect_to request.referer, notice: 'Review successfully updated.'
+    else
+      redirect_to request.referer, alert: 'There was an error updating your review.'
+    end
+  end
+
+  # Menghapus review
+  def destroy
+    @review.destroy
+    redirect_to request.referer, notice: 'Review successfully deleted.'
+  end
+
+  private
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def set_review
+    @review = @course.reviews.find_by(user: current_user) # Menemukan review yang dimiliki oleh current_user
+  end
   def review_params
     params.require(:review).permit(:rating, :content)
   end
