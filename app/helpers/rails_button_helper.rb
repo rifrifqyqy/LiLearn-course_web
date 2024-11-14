e848aae77251ac67b05913bc113fa2b7db2a8acd
rifrifqyqy <rifqyh22@gmail.com>
2024-11-14 21:40:54 +0700

    add method to helper

--- app/helpers/rails_button_helper.rb
+++ app/helpers/rails_button_helper.rb
@@ -6,23 +6,20 @@ module RailsButtonHelper
   # padding for padding
 
 
-
-  def railsbutton_primary(text = "Primary Button", url = "#", color: "bg-zinc-800 text-white", padding: "px-4 py-2", btn_class: nil, icon: false)
+  def railsbutton_primary(text = "Primary Button", url = "#", color: "bg-zinc-800 text-white", padding: "px-4 py-2", btn_class: nil, icon: false, method: :get)
     base_class = "font-medium flex items-center justify-between gap-2 hover:opacity-75 transition-all rounded-full w-fit active:scale-95"
-    # Merge class variable
     full_class = "#{color} #{padding} #{base_class} #{btn_class}"
-    
-    # Render button with optional icon
+  
     if icon.present?
       icon_html = content_tag(:span, class: "items-center") do
-        # Menggunakan raw untuk menampilkan HTML jika icon adalah string HTML
         raw(icon)
       end
-      link_to "#{text}#{icon_html}".html_safe, url, class: full_class.strip
+      link_to "#{text}#{icon_html}".html_safe, url, method: method, class: full_class.strip, data: { confirm: 'Are you sure?' }
     else
-      link_to text, url, class: full_class.strip
+      link_to text, url, method: method, class: full_class.strip, data: { confirm: 'Are you sure?' }
     end
   end
+  
   
 
   def railsbutton_secondary(text = "Secondary Button", url = "#", color: "bg-zinc-200 text-zinc-800", padding: "px-4 py-2", btn_class: nil)
